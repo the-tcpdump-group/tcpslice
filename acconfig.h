830d4b3cbd60e300ed40ec0865906ed0904e3daf
fenner@gmail.com <>
2001-11-12 22:41:32 +0000

    Add "const" for autoconf 2.52 compatability (for now)

--- acconfig.h
+++ acconfig.h
@@ -18,3 +18,11 @@
 /* If you don't have sized types, define these to be the correct sizes. */
 #undef	int32_t
 #undef	u_int32_t
+
+/* Long story short: aclocal.m4 depends on autoconf 2.13
+ * implementation details wrt "const"; newer versions
+ * have different implementation details so for now we
+ * put "const" here.  This may cause duplicate definitions
+ * in config.h but that should be OK since they're the same.
+ */
+#undef const
